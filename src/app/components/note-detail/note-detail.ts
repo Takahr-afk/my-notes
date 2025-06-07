c4c15f24e038e1f7606d4ee5b4bc6c203171e1a5
Jannik Andres <ja075822@fh-muenster.de>
2025-06-07 10:05:40 +0200

    added components & auth

---
+++ src/app/components/note-detail/note-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-detail',
+  imports: [],
+  templateUrl: './note-detail.html',
+  styleUrl: './note-detail.css'
+})
+export class NoteDetail {
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, user } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  user$ = user(this.auth);
+
+  constructor(private auth: Auth) {}
+
+  async login(email: string, password: string): Promise<void> {
+    await signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async register(email: string, password: string): Promise<void> {
+    await createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  async logout(): Promise<void> {
+    await signOut(this.auth);
+  }
+}
